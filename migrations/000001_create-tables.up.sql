1b5d10b35f48ed89b05fd94b026c6710b62f098f
samuraivf <vovka4034@gmail.com>
2023-06-12 18:25:42 +0300

    Add tasks table in migration files

--- migrations/000001_create-tables.up.sql
+++ migrations/000001_create-tables.up.sql
@@ -10,11 +10,26 @@ CREATE TABLE projects (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     description TEXT,
-    admin INT REFERENCES users(id) ON DELETE CASCADE NOT NULL
+    admin INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE projects_members (
     project_id INT REFERENCES projects(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     member_id INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     CONSTRAINT id PRIMARY KEY (project_id, member_id)
+);
+
+CREATE TYPE priority AS ENUM ('low', 'medium', 'high');
+CREATE TYPE progress AS ENUM ('TO DO', 'IN PROGRESS', 'IN REVIEW', 'DONE');
+
+CREATE TABLE tasks (
+    id BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    task_priority priority,
+    assignee INT REFERENCES users(id) ON DELETE CASCADE,
+    project_id INT REFERENCES projects(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    task_type progress,
+    created_at TIMESTAMP NOT NULL,
+    perform_to TIMESTAMP
 );
